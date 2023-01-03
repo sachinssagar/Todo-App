62a7b78c1031ebd585659265037556be412cb333
Sachin Sagar <sachinssagar@gmail.com>
2023-01-03 23:27:38 +0530

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Header } from "./components/Header";
+import Form from "./components/Form";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { App };
